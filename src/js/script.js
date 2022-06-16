9b2bfcf61a8111ba2545a7476fc7dc517c002aac
Dgley ramos <dgleysilva@gmail.com>
2022-06-16 01:36:48 -0300

    Menu para desktop criado

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,9 @@ let dropdown_connect = document.getElementById("button_connect");
 let connect_arrow = document.getElementById("connect_arrow");
 let connect = document.getElementById("connect");
 
+
+let min_width = window.matchMedia("(min-width: 768px)")
+
 menu.addEventListener('click', (e) => {
   if(menu_bar.src.includes("src/img/icon-hamburger.svg")){
     menu_bar.src = "../../src/img/icon-close.svg";
@@ -29,15 +32,44 @@ menu.addEventListener('click', (e) => {
 dropdown_product.addEventListener('click', (e) =>  {
   product_arrow.classList.toggle("girar");
   product.classList.toggle("aparecer");
+  if(company.classList.contains("aparecer")){
+    company.classList.toggle("aparecer");
+  }
+  if(connect.classList.contains("aparecer")){
+    connect.classList.toggle("aparecer");
+  }
 });
 
 dropdown_company.addEventListener('click', (e) =>  {
   company_arrow.classList.toggle("girar");
   company.classList.toggle("aparecer");
+  if(product.classList.contains("aparecer")){
+    product.classList.toggle("aparecer");
+  }
+  if(connect.classList.contains("aparecer")){
+    connect.classList.toggle("aparecer");
+  }
 });
 
 
 dropdown_connect.addEventListener('click', (e) =>  {
   connect_arrow.classList.toggle("girar");
   connect.classList.toggle("aparecer");
+  if(product.classList.contains("aparecer")){
+    product.classList.toggle("aparecer");
+  }
+  if(company.classList.contains("aparecer")){
+    company.classList.toggle("aparecer");
+  }
 });
+
+if (min_width.matches) {
+  connect_arrow.src = "./../../src/img/icon-arrow-light.svg";
+  product_arrow.src = "./../../src/img/icon-arrow-light.svg";
+  company_arrow.src = "./../../src/img/icon-arrow-light.svg";
+} else {
+  connect_arrow.src = "./../../src/img/icon-arrow-dark.svg";
+  product_arrow.src = "./../../src/img/icon-arrow-dark.svg";
+  company_arrow.src = "./../../src/img/icon-arrow-dark.svg";
+}
+
